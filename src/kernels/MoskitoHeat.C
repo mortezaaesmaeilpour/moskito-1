f0124ebc4d3c0b9cb662ee336c42fc17d34d5c05
= <=>
2019-07-17 14:27:33 +0200

    Adapt lateral heat Kernel to changes in Materials - Temperature Wellbore Formation Boundary is now MaterialProperty

--- src/kernels/MoskitoHeat.C
+++ src/kernels/MoskitoHeat.C
@@ -41,7 +41,7 @@ MoskitoHeat::MoskitoHeat(const InputParameters & parameters)
   _T(getMaterialProperty<Real>("temperature")),
   _rto(getMaterialProperty<Real>("radius_tubbing_outer")),
   _Uto(getMaterialProperty<Real>("thermal_resistivity_well")),
-  _Twb(getMaterialProperty<Real>("temperature_formation_well_boundary"))
+  _Twb(getMaterialProperty<Real>("temperature_well_formation_interface"))
   {
   }
 
@@ -54,8 +54,8 @@ MoskitoHeat::computeQpResidual()
   r *= ((_T[_qp] * gradC_to_gradR) - _Twb[_qp]);
   r /= Watt_to_Btu_per_h;
   r /= m_to_ft;
-  std::cout<<"z coord ="<<_q_point[_qp] <<std::endl;
-  std::cout<<"_r = "<<r<<std::endl;
+  // std::cout<<"z coord ="<<_q_point[_qp] <<std::endl;
+  // std::cout<<"_r = "<<r<<std::endl;
   // std::cout<<"_Uto = "<<_Uto[_qp]<<std::endl;
   // std::cout<<"_T = "<<_T[_qp] * gradC_to_gradR + Rankine_absol<<std::endl;
   // std::cout<<"_Twb = "<<_Twb[_qp]<<std::endl;
